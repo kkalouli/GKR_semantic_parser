65393e4c3168c02f269630834c287e2d2600401e
kkalouli <Aikaterini-Lida.Kalouli@uni-konstanz.de>
2017-10-24 15:10:24 +0200

    fixing build

---
+++ sem.graph/src/main/java/semantic/graph/vetypes/LexEdgeContent.java
@@ -0,0 +1,18 @@
+package semantic.graph.vetypes;
+
+import java.io.Serializable;
+
+import semantic.graph.EdgeContent;
+
+/**
+ * The {@link EdgeContent} for {@link LexEdge}s
+  *
+ */
+public class LexEdgeContent implements EdgeContent, Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -5865810013037527830L;
+
+}
---
+++ sem.graph/src/main/java/semantic/graph/vetypes/TermNodeContent.java
@@ -0,0 +1,18 @@
+package semantic.graph.vetypes;
+
+import java.io.Serializable;
+
+import semantic.graph.NodeContent;
+
+/**
+ * The {@link NodeContent} of a {@link TermNode}
+ *
+ */
+public class TermNodeContent extends NodeContent implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -6298888883067719047L;
+
+}
